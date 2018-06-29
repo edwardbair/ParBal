9e7fb77a9a634d1b99ebb978023e64929cad3f5b
edwardbair <nbair@eri.ucsb.edu>
2018-06-29 18:07:54 +0100

    change to downscale energy

--- downscale_energy.m
+++ downscale_energy.m
@@ -1,5 +1,5 @@
 function downscale_energy(sFileDay,sFile,topofile,landcoverfile,...
-    ldas_dir,ldas_dem_file,ceres_dir,ceres_topofile,fast_flag,outdir,varargin)
+    ldas_dir,ldas_topo_file,ceres_dir,ceres_topofile,fast_flag,outdir,varargin)
 % downscales energy for a day, specifically meant for running on individual
 % workers on Azure. Contains a parfor loop for computing snow surface
 % temperatures
@@ -10,7 +10,7 @@ function downscale_energy(sFileDay,sFile,topofile,landcoverfile,...
 % topofile - path to h5 topofile created by TopoHorizons
 % landcoverfile - path to h5 or mat land cover file
 % ldas_dir - base path to ldas files, don't specify a year
-% ldas_dem_file - path to h5 or mat ldas dem
+% ldas_topo_file - path to h5 or mat ldas topofile
 % ceres_dir - base path to ceres files
 % ceres_topofile - path to ceres topofile (mat - h5 not implemented yet)
 % fast flag - true - only solve for M; false - solve for all outputs; 
@@ -51,7 +51,7 @@ if nargin==12
     metvars_flag=varargin{2};
 end
 
-parse(p,sFileDay,sFile,topofile,landcoverfile,ldas_dir,ldas_dem_file,...
+parse(p,sFileDay,sFile,topofile,landcoverfile,ldas_dir,ldas_topo_file,...
     ceres_dir,ceres_topofile,outdir,LDASOnlyFlag,metvars_flag)
 
 
@@ -63,7 +63,7 @@ end
 % get required variables and filenames
 [FOREST, topo, ldas, ldas_topo, dateval, sFile, ceres,...
     ceres_topo,tz,outfile]=include_vars_melt(sFileDay,sFile,topofile,...
-    landcoverfile,ldas_dir,ldas_dem_file,ceres_dir,ceres_topofile,outdir,...
+    landcoverfile,ldas_dir,ldas_topo_file,ceres_dir,ceres_topofile,outdir,...
     LDASOnlyFlag);
 
 %run downscaling
